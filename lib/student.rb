73d57d0e3b4441e76ca018e23a8531c36d31736a
Carla Holcomb <carla.programmer@gmail.com>
2020-01-02 03:24:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,16 @@ attr_reader :id
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+  
+    def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 
 end
