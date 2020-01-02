86a8e5471dbaf033937c43402fa15b5b9510e207
Carla Holcomb <carla.programmer@gmail.com>
2020-01-02 03:17:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ attr_reader :id
   end
  
   def self.find_by_name(name)
-    sql = "SELECT * FROM songs WHERE name = ?"
+    sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Song.new(result[0], result[1], result[2])
   end
