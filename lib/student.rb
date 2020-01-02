bf3c9b5d4ef37aa20bb98e4f597a1fd0229b54d4
Carla Holcomb <carla.programmer@gmail.com>
2020-01-02 03:16:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,13 +4,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name, :album
+  attr_accessor :name, :grade
 attr_reader :id
  
-  def initialize(id=nil, name, album)
+  def initialize(id=nil, name, grade)
     @id = id
     @name = name
-    @album = album
+    @grade = grade
   end
  
   def self.create_table
