205160f16ec7e4f76ed56d006260ba363357b8dc
Carla Holcomb <carla.programmer@gmail.com>
2020-01-02 03:17:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,8 @@ attr_reader :id
       VALUES (?, ?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
  
   def self.create(name:, album:)
